2b900fad9fd4259465f32af1df494b8feedbcf11
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-08 15:50:40 -0700

    second test fail

--- index.test.js
+++ index.test.js
@@ -1,12 +1,24 @@
 const getBook = require("./index")
 
-test ('show the Empty book List',() =>{
+// test ('show the Empty book List',() =>{
 
+//     //Arrange
+//     const booklist =[]
+
+//     // Act
+//     const result = getBook(booklist)
+//     //Assert
+//     expect(result).toEqual([]) 
+// })
+
+test('return the incremented list',()=>{
+    
     //Arrange
     const booklist =[]
+    let numberRead = 0
 
     // Act
-    const result = getBook(booklist)
+    const result = getBook(booklist, numberRead)
     //Assert
-    expect(result).toEqual([]) 
+    expect(result).toBe(1)
 })
