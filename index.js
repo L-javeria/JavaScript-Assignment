714b696e13b987cf097315022ccaf567f4dfad3b
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-08 15:52:14 -0700

    incremented list

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // console.log('name')
 
-function getBook(booklist){
-    
+function getBook(booklist,numberRead){
+    let bookName = 'The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****'
+    booklist.push(bookName)
+    if (booklist.includes(bookName)){
+        return numberRead+=1
+    } else {
     return []
+}
 }
 
 module.exports = getBook
