45bf39d7a3db66bc0b02346cddb36f80ff9b65ae
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-09 10:26:14 -0700

    3rd test fail

--- Testcases/unitTesting.test.js
+++ Testcases/unitTesting.test.js
@@ -12,3 +12,28 @@ test('Return a Empty List', ()=>{
     //Assert
     expect(list).toEqual(bookList)
 }) 
+
+/*3 - Given that I have an empty list, when I add additional books to the list 
+I expect the numberRead to return the total number of books in my list.*/
+
+test('Read Additional Books In List' , ()=>{
+ 
+    //Arrange
+    const bookDetails = {
+      book1:{ title:"prey" , author:"jenny" , page: 230 , year:2000},
+      book2:{ title:"alculus" , author:"Howord Anton" , page: 600 , year:2012}
+   }
+    const bookinfor = {
+      book1_intro:{readDate: "21 March 2030" , rating: 3},
+      book2_intro:{readDate: "21 March 2030" , rating: 4}
+  }
+    addBooks(bookDetails.book1,bookinfor.book1_intro);
+    addBooks(bookDetails.book2,bookinfor.book2_intro)
+    const result = 2;
+
+  //Act
+ const axpected = numRead();
+
+    // Assert
+    expect(axpected).toBe(result)
+})
