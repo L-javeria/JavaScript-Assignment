6a9732d50ad1341d4d8c80bae53f3052a5749abd
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-09 10:17:14 -0700

    1st test fail

---
+++ Testcases/unitTesting.test.js
@@ -0,0 +1,14 @@
+// Written BY Muhammad Atique
+
+const  { getBook, numRead ,addBooks} = require('../readingList');
+
+/Given that I visit the site, when I first start, I expect my list to be empty./
+
+test('Return a Empty List', ()=>{
+    // Arrange
+    const bookList = [];
+    //Act
+    const list = getBook();
+    //Assert
+    expect(list).toEqual(bookList)
+}) 
