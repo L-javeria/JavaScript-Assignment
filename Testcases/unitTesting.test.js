06abc0b69413447d20aae769eba57c1dab9b2404
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-09 10:23:29 -0700

    1st test passed

--- Testcases/unitTesting.test.js
+++ Testcases/unitTesting.test.js
@@ -1,6 +1,6 @@
 // Written BY Muhammad Atique
 
-const  { getBook, numRead ,addBooks} = require('../readingList');
+const  { getBook, numRead ,addBooks} = require('../Testfunction/readingList');
 
 /Given that I visit the site, when I first start, I expect my list to be empty./
 
