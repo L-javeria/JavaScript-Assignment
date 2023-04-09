16cce0fabee5cd345388750261352f2b39b6127a
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-09 09:21:04 -0700

    6th test fail

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const getBooksByRatings = require("./index")
 const getBook = require("./index")
 
 // test ('show the Empty book List',() =>{
@@ -23,13 +24,28 @@ const getBook = require("./index")
 //     expect(result).toBe(1)
 // })
 
-test ('Delete the book',()=>{
-    // Arrange
-    const booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****',
-    'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992,****']
+// test ('Delete the book',()=>{
+//     // Arrange
+//     const booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****',
+//     'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992,****']
+
+//     // Act
+//     const result = getBook(booklist)
+//     //Assert
+//     expect(result).toBe(booklist)
+// })
+
+test('Returns books by ratings',()=> {
+    
+    //Arrange
+    let booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, ****',
+    'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992, *****',
+    'Pshychology of Money By Morgan Housel, 256']
 
     // Act
-    const result = getBook(booklist)
+    const result = getBooksByRatings(booklist)
+
     //Assert
-    expect(result).toBe(booklist)
+    let expected_result = ['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, ****']
+    expect(expected_result).toBe(result)
 })
