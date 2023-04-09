a4f76a6ce0114178700d7d347960873bb28a8acd
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-08 22:43:55 -0700

    fourth test fail

--- index.test.js
+++ index.test.js
@@ -11,14 +11,25 @@ const getBook = require("./index")
 //     expect(result).toEqual([]) 
 // })
 
-test('return the incremented list',()=>{
+// test('return the incremented list',()=>{
     
-    //Arrange
-    const booklist =[]
-    let numberRead = 0
+//     //Arrange
+//     const booklist =[]
+//     let numberRead = 0
+
+//     // Act
+//     const result = getBook(booklist, numberRead)
+//     //Assert
+//     expect(result).toBe(1)
+// })
+
+test ('Delete the book',()=>{
+    // Arrange
+    const booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****',
+    'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992,****']
 
     // Act
-    const result = getBook(booklist, numberRead)
+    const result = getBook(booklist)
     //Assert
-    expect(result).toBe(1)
+    expect(result).toBe(booklist)
 })
