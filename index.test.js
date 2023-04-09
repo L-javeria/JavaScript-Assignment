ab78579858365f3a17ffcb394a6fc22d8e4a8855
Muhammad Atique <123871250+Md-Atique-Github@users.noreply.github.com>
2023-04-09 21:31:43 +0500

    update

--- index.test.js
+++ index.test.js
@@ -1,39 +1,41 @@
+
+ // Written by Muhammad Atique and Javeria Lodhi..
+
 const getBooksByRatings = require("./index")
 const getBook = require("./index")
 
-// test ('show the Empty book List',() =>{
+ test ('show the Empty book List',() =>{
 
-//     //Arrange
-//     const booklist =[]
+     //Arrange
+     const booklist =[]
+     // Act
+     const result = getBook(booklist)
+     //Assert
+     expect(result).toEqual([]) 
+ })
 
-//     // Act
-//     const result = getBook(booklist)
-//     //Assert
-//     expect(result).toEqual([]) 
-// })
-
-// test('return the incremented list',()=>{
+ test('return the incremented list',()=>{
     
-//     //Arrange
-//     const booklist =[]
-//     let numberRead = 0
+     //Arrange
+     const booklist =[]
+     let numberRead = 0
 
-//     // Act
-//     const result = getBook(booklist, numberRead)
-//     //Assert
-//     expect(result).toBe(1)
-// })
+     // Act
+     const result = getBook(booklist, numberRead)
+     //Assert
+     expect(result).toBe(1)
+ })
 
-// test ('Delete the book',()=>{
-//     // Arrange
-//     const booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****',
-//     'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992,****']
+ test ('Delete the book',()=>{
+     // Arrange
+     const booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****',
+     'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992,****']
 
-//     // Act
-//     const result = getBook(booklist)
-//     //Assert
-//     expect(result).toBe(booklist)
-// })
+     // Act
+     const result = getBook(booklist)
+     //Assert
+     expect(result).toBe(booklist)
+ })
 
 test('Returns books by ratings',()=> {
     
@@ -46,8 +48,6 @@ test('Returns books by ratings',()=> {
     const result = getBooksByRatings(booklist)
 
     //Assert
-    let expected_result = ['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, ****',
-    'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992, *****']
-    
-    expect(expected_result).toEqual(result)
+    let expected_result = ['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, ****']
+    expect(expected_result).toBe(result)
 })
