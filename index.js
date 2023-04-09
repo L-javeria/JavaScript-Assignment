bc41595ece6c2ddbe70ba43406188fd8af98097f
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-09 09:33:27 -0700

    javeria's test case

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// console.log('name')
+// code written by javeria lodhi
 
 // # First Test: Return the empty String List
-// function getBook(booklist){
-//     return []
-// }
+function getBook(booklist){
+    return []
+}
 
 // # Second Test : Incremented List
-// function getBook(booklist,numberRead){
-//     let bookName = 'The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****'
-//     booklist.push(bookName)
-//     if (booklist.includes(bookName)){
-//         return numberRead+=1
-//     } else {
-//     return []
-// }
-// }
+function getBook(booklist,numberRead){
+    let bookName = 'The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****'
+    booklist.push(bookName)
+    if (booklist.includes(bookName)){
+        return numberRead+=1
+    } else {
+    return []
+}
+}
 
 //Fourth Test: delete the book
 function getBook(booklist){
--- index.test.js
+++ index.test.js
@@ -1,41 +1,43 @@
+// code written by javeria lodhi
+
 const getBooksByRatings = require("./index")
 const getBook = require("./index")
 
-// test ('show the Empty book List',() =>{
+test ('show the Empty book List',() =>{             //1st test case
 
-//     //Arrange
-//     const booklist =[]
+    //Arrange
+    const booklist =[]
 
-//     // Act
-//     const result = getBook(booklist)
-//     //Assert
-//     expect(result).toEqual([]) 
-// })
+    // Act
+    const result = getBook(booklist)
+    //Assert
+    expect(result).toEqual([]) 
+})
 
-// test('return the incremented list',()=>{
+test('return the incremented list',()=>{           //2nd test case
     
-//     //Arrange
-//     const booklist =[]
-//     let numberRead = 0
+    //Arrange
+    const booklist =[]
+    let numberRead = 0
 
-//     // Act
-//     const result = getBook(booklist, numberRead)
-//     //Assert
-//     expect(result).toBe(1)
-// })
+    // Act
+    const result = getBook(booklist, numberRead)
+    //Assert
+    expect(result).toBe(1)
+})
 
-// test ('Delete the book',()=>{
-//     // Arrange
-//     const booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****',
-//     'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992,****']
+test ('Delete the book',()=>{                 //4th test case
+    // Arrange
+    const booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****',
+    'Alices Adventures in Wonderland by Lewis Carroll, 544 pages, 1865, read on May 22, 1992,****']
 
-//     // Act
-//     const result = getBook(booklist)
-//     //Assert
-//     expect(result).toBe(booklist)
-// })
+    // Act
+    const result = getBook(booklist)
+    //Assert
+    expect(result).toBe(booklist)
+})
 
-test('Returns books by ratings',()=> {
+test('Returns books by ratings',()=> {           //6th test case
     
     //Arrange
     let booklist =['The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, ****',
@@ -51,3 +53,4 @@ test('Returns books by ratings',()=> {
     
     expect(expected_result).toEqual(result)
 })
+
