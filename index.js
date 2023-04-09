e2cf13f3971b081962722f578aa751690138a369
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-08 22:45:12 -0700

    Fourth test passed

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 // console.log('name')
 
-function getBook(booklist,numberRead){
-    let bookName = 'The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****'
-    booklist.push(bookName)
-    if (booklist.includes(bookName)){
-        return numberRead+=1
-    } else {
-    return []
-}
-}
+// # First Test: Return the empty String List
+// function getBook(booklist){
+//     return []
+// }
+
+// # Second Test : Incremented List
+// function getBook(booklist,numberRead){
+//     let bookName = 'The Hobbit by J.R.R. Tolkein, 320 pages, 1937 read on January 1, 2020, *****'
+//     booklist.push(bookName)
+//     if (booklist.includes(bookName)){
+//         return numberRead+=1
+//     } else {
+//     return []
+// }
+// }
+
+//Fourth Test: delete the book
+function getBook(booklist){
+    let bookName = 'The Hobbit'
+    let book = booklist.findIndex(item => item.includes(bookName))
+      if (book !== -1){
+      let result = booklist.splice(book, 1)
+      return booklist
+    }
+} 
 
 module.exports = getBook
