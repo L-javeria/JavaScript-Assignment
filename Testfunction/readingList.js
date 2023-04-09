c538d7f10c870bf5dd855602818ef0f85db122c0
JL-2303-khi-cnd-006 <javeria.lodhi@xloopdigital.com>
2023-04-09 10:30:33 -0700

    3rd test passed

--- Testfunction/readingList.js
+++ Testfunction/readingList.js
@@ -3,3 +3,12 @@ let readingList = []
 exports.getBook = ()=>{
     return readingList
 }
+
+exports.addBooks= (bookDetails,readDate,rating) =>{
+   bookDetails.readDate=readDate;
+   bookDetails.rating=rating;
+   readingList.push({bookDetails});
+}
+exports.numRead=() => {
+    return readingList.length;
+}
